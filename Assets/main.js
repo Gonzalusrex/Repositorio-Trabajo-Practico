63e11135a635585934d1ca9899863dcda6f837f6
gonzalusrex <gonzaloverdugo91@gmail.com>
2026-01-12 15:09:34 -0300

    actualizacion de estado de trabajo para acceder al proyecto en el otro computador

--- Assets/main.js
+++ Assets/main.js
@@ -1,3 +1,103 @@
+//Secion de Scripts para la creacion de usuarios
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+const registerBtn = document.getElementById("registerBtn");
+
+//si existe el boto nde registro aniade el evento click para escribir los nuevos usuarios//
+
+if (registerBtn) {
+    registerBtn.addEventListener("click", () => {
+        const user = document.getElementById("regUser").value.trim().toLowerCase();
+        const pass = document.getElementById("regPass").value.trim();
+
+        if (!user || !pass) {
+            alert("Complete todos los campos");
+            return;
+        }
+
+        if (!isValidEmail(user)) {
+            alert("Ingrese un correo electrónico válido");
+            return;
+        }
+
+        const exists = users.some(u => u.user === user);
+        if (exists) {
+            alert("El usuario ya existe");
+            return;
+        }
+
+        users.push({ user, pass });
+        localStorage.setItem("users", JSON.stringify(users));
+
+        alert("Usuario creado correctamente");
+        window.location.href = "login.html";
+    });
+}
+
+function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+
+
+
+
+
+
+
+
+// Seccion de Scripts para el manejo del saldo en diferentes vistas //
+
+let balance = Number(localStorage.getItem("balance")) || 0;
+
+
+function saveBalance() {
+    localStorage.setItem("balance", balance);
+}
+
+function renderBalance() {
+    const balanceElements = document.querySelectorAll(".balance-actual");
+
+    balanceElements.forEach(el => {
+        el.textContent = `$${balance}`;
+    });
+}
+
+
+const depositBtn = document.getElementById("depositBtn");
+const montodeposito = document.getElementById("montodeposito");
+
+if (depositBtn) {
+    depositBtn.addEventListener("click", () => {
+        const monto = Number(montodeposito.value);
+
+        if (monto <= 0 || isNaN(monto)) {
+            alert("Ingrese un monto válido");
+            return;
+        }
+
+        balance += monto;
+        saveBalance();
+        renderBalance();
+
+        montodeposito.value = "";
+    });
+}
+
+
+renderBalance();
+
+
+
+
+
+
+
+
+
+
+
+
 //Seccion de scripts para vista de Contactos//
 
 let contactos = JSON.parse(localStorage.getItem("contactos")) || [];
@@ -53,18 +153,12 @@ if (addBtn) {
         const banco = bankInput.value.trim();
 
         if (nombre === "" || apellido === "" || banco === "") {
-        alert("Complete todos los campos");
-        return;
-}
+            alert("Complete todos los campos");
+            return;
+        }
 
 
- //Funciones js para vista de contactos//
-
-
-
-
-
-// Validar si ya existe el contacto//
+        // Validar si ya existe el contacto//
 
         const exists = contactos.some(
             contact => contact.nombre === nombre
@@ -75,12 +169,12 @@ if (addBtn) {
         }
 
         contactos.push({ nombre, apellido, banco });
-    saveContactos();
-    renderContactos();
+        saveContactos();
+        renderContactos();
 
-    nameInput.value = "";
-    apellidoInput.value = "";
-    bankInput.value = "";
+        nameInput.value = "";
+        apellidoInput.value = "";
+        bankInput.value = "";
     });
 }
 
