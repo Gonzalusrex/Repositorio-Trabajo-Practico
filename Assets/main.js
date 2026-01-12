e169c9122a548f923870e8aab142c5896090165e
gonzalusrex <gonzaloverdugo91@gmail.com>
2026-01-11 22:34:50 -0300

    Aplicacion de estilos generales a las vistas

--- Assets/main.js
+++ Assets/main.js
@@ -1,23 +1,72 @@
-// script para agregar contactos en vista contactos //
-let contacts = [];
+//Seccion de scripts para vista de Contactos//
 
+let contactos = JSON.parse(localStorage.getItem("contactos")) || [];
+
+
+//Obtener elementos del DOM//
 
 const addBtn = document.getElementById("addContact");
 const nameInput = document.getElementById("contactName");
+const apellidoInput = document.getElementById("contactLastName");
 const bankInput = document.getElementById("contactBank");
 const table = document.getElementById("contactTable");
 
+
+
+// Muestra los contactos Existentes //
+
+function renderContactos() {
+    table.innerHTML = "";
+
+    contactos.forEach((contact, index) => {
+        table.innerHTML += `
+            <tr>
+                <td>${contact.nombre}</td>
+                <td>${contact.apellido}</td>
+                <td>${contact.banco}</td>
+                <td>
+                    <button class="btn btn-danger btn-sm"
+                        onclick="deleteContact(${index})">
+                        Eliminar
+                    </button>
+                </td>
+            </tr>
+        `;
+    });
+}
+
+function deleteContact(index) {
+    contactos.splice(index, 1);
+    saveContactos();
+    renderContactos();
+}
+
+function saveContactos() {
+    localStorage.setItem("contactos", JSON.stringify(contactos));
+}
+
+// Agregar contactos y valida que se llenen los campos//
 if (addBtn) {
     addBtn.addEventListener("click", () => {
         const nombre = nameInput.value.trim();
+        const apellido = apellidoInput.value.trim();
         const banco = bankInput.value.trim();
 
-        if (nombre === "" || banco === "") {
-            return alert("Complete todos los campos");
-        }
+        if (nombre === "" || apellido === "" || banco === "") {
+        alert("Complete todos los campos");
+        return;
+}
 
-        // Validar si ya existe el contacto
-        const exists = contacts.some(
+
+ //Funciones js para vista de contactos//
+
+
+
+
+
+// Validar si ya existe el contacto//
+
+        const exists = contactos.some(
             contact => contact.nombre === nombre
         );
 
@@ -25,11 +74,19 @@ if (addBtn) {
             return alert("El contacto ya existe");
         }
 
-        contacts.push({ nombre, banco });
+        contactos.push({ nombre, apellido, banco });
+    saveContactos();
+    renderContactos();
 
-        nameInput.value = "";
-        bankInput.value = "";
-
-        renderContacts();
+    nameInput.value = "";
+    apellidoInput.value = "";
+    bankInput.value = "";
     });
 }
+
+// linea para mostrar el array de contactos en la vista //
+renderContactos();
+
+
+
+//---- Transferencias -----//
